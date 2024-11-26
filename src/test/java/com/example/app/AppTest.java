6a458ddfafe7c60ee5481680dbc5a282debb25fb
Jaypal Singh Sisodiya <jaypals840@gmail.com>
2024-11-26 21:09:39 +0530

    first commit

---
+++ src/test/java/com/example/app/AppTest.java
@@ -0,0 +1,11 @@
+package com.example.app;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue("The app should run properly", true);
+    }
+}
